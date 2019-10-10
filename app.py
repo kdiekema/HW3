c2a862097ff22836aed171ee9bb8ad7e262620d0
kdiekema <karina-diekema@uiowa.edu>
2019-10-10 09:59:25 -0500

    first calculator draft without currency formatting and css

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', display="", pageTitle = 'My Loan Calculator')
+
+@app.route('/calculate', methods=['GET', 'POST'])
+def calculate():
+    if request.method == 'POST':
+        form = request.form
+        a = float(form['a'])
+        n = float(form['n'])
+        i = float(form['i'])
+        d = (((1+i)**n)-1)/(((1+i)**n)*i)
+        calculate = (a/(d))
+        return render_template('index.html', display=calculate, pageTitle="My Loan Calculator")
+
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
